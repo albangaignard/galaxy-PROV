8aa317fc9193fcc3f2ab8c5f1512c60ced5ef3dc
Alban Gaignard <aban.gaignard@univ-nantes.fr>
2017-03-14 12:40:29 +0100

    added -d to print debug information dump PROV in TTL to the standard output

--- GalaxyLD/src/main/java/fr/univnantes/galaxyld/Main.java
+++ GalaxyLD/src/main/java/fr/univnantes/galaxyld/Main.java
@@ -23,14 +23,7 @@
  */
 package fr.univnantes.galaxyld;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
-import java.util.logging.Level;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -39,6 +32,7 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang.time.StopWatch;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
@@ -60,12 +54,14 @@ public class Main {
         Option gUrlOpt = new Option("u", "galaxyUrl", true, "the Galaxy server URL");
         Option gHistIDOpt = new Option("i", "galaxyHistoryID", true, "the Galaxy history ID used to extract PROV RDF statements");
         Option versionOpt = new Option("v", "version", false, "print the version information and exit");
+        Option debugOpt = new Option("d", "debug", false, "print the debuging information");
         Option helpOpt = new Option("h", "help", false, "print the help");
         options.addOption(listGHistOpt);
         options.addOption(apiKeyOpt);
         options.addOption(gHistIDOpt);
         options.addOption(gUrlOpt);
         options.addOption(versionOpt);
+        options.addOption(debugOpt);
         options.addOption(helpOpt);
 
         String header = "GalaxyLD is a tool to extract provenance information from Galaxy workspaces through the PROV-O ontology.";
@@ -81,6 +77,12 @@ public class Main {
                 System.exit(0);
             }
 
+            if (cmd.hasOption("d")) {
+                Logger.getLogger("fr").setLevel(Level.ALL);
+            } else {
+                Logger.getLogger("fr").setLevel(Level.OFF);
+            }
+
             if (cmd.hasOption("v")) {
                 logger.info("GalaxyLD version 1.0-SNAPSHOT");
                 System.exit(0);
@@ -91,7 +93,7 @@ public class Main {
             } else {
                 logger.warn("Please specify your Galaxy API key !");
                 HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("ga2prov", header, options, footer, true);
+                formatter.printHelp("galaxy-PROV", header, options, footer, true);
                 System.exit(0);
             }
 
@@ -113,8 +115,9 @@ public class Main {
                     gAPI = GHistFactory.getInstance();
                     Map<String, String> histories = gAPI.listHistories();
                     for (String id : histories.keySet()) {
-                        String message = String.format("Found history with id %s and name %s", id, histories.get(id));
-                        System.out.println("\t"+message);
+//                        String message = String.format("Found history with id %s and name %s", id, histories.get(id));
+                        String message = String.format("ID: %s, LABEL: %s", id, histories.get(id));
+                        System.out.println(message);
                     }
                     System.exit(0);
                 } catch (GalaxyProvenanceException ex) {
@@ -124,77 +127,20 @@ public class Main {
             }
 
             if (cmd.hasOption("i")) {
+                logger.info("Fetching PROV triples");
                 StopWatch sw = new StopWatch();
                 sw.start();
-
                 Main.gHist = cmd.getOptionValue("i");
-
                 GHistAPI gAPI = GHistFactory.getInstance();
                 String provTTL = gAPI.getProv(Main.gHist);
+                System.out.println(provTTL);
                 sw.stop();
                 logger.info("PROV triples retrieved in " + sw.getTime() + " ms");
-                File oF = File.createTempFile("PROV-", ".ttl", Paths.get(".").toFile());
-                try (BufferedWriter writer = Files.newBufferedWriter(oF.toPath())) {
-                    writer.write(provTTL);
-                }
-                logger.info("PROV written to " + Paths.get(oF.toURI()).normalize());
-
-//                GraphStore graph = GraphStore.create(false);
-//                QueryProcess exec = QueryProcess.create(graph);
-//                Load ld = Load.create(graph);
-//                InputStream inputStream = new ByteArrayInputStream(provTTL.getBytes(StandardCharsets.UTF_8));
-//                ld.load(inputStream,".ttl");
-//                logger.info("Loaded " + graph.size() + " rdf statements.");
-//
-//                Mappings maps = null;
-//
-//                if (!Main.isFull) {
-//                    String filterQuery = "PREFIX prov:<http://www.w3.org/ns/prov#>\n"
-//                            + "CONSTRUCT {\n"
-//                            + "	?x ?p ?y .\n"
-//                            + " ?x rdfs:label ?xL .\n"
-//                            + " ?y rdfs:label ?yL .\n"
-//                            + "} WHERE {\n"
-//                            + "	?x ?p ?y .\n"
-//                            + " OPTIONAL {?x rdfs:label ?xL} .\n"
-//                            + " OPTIONAL {?y rdfs:label ?yL} .\n"
-//                            //                        + "	FILTER (?p NOT IN (rdf:type, prov:wasGeneratedBy, prov:qualifiedAssociation, prov:hadPlan, prov:agent, rdfs:comment)) \n"
-////                            + "	FILTER (?p IN (prov:wasDerivedFrom, rdfs:label, prov:wasAttributedTo, prov:startedAtTime, prov:endedAtTime )) \n"
-//                            + "	FILTER (?p IN (prov:wasDerivedFrom, rdfs:label, prov:wasAttributedTo)) \n"
-//                            + "} ";
-//                    maps = exec.query(filterQuery);
-//                } else {
-//                    String filterQuery = "PREFIX prov:<http://www.w3.org/ns/prov#>\n"
-//                            + "CONSTRUCT {\n"
-//                            + "	?x ?p ?y .\n"
-//                            + "} WHERE {\n"
-//                            + "	?x ?p ?y .\n"
-//                            //                        + "	FILTER (?p NOT IN (rdf:type, prov:wasGeneratedBy, prov:qualifiedAssociation, prov:hadPlan, prov:agent, rdfs:comment)) \n"
-////                            + "	FILTER (?p NOT IN (rdf:type)) \n"
-//                            + "} ";
-//                    maps = exec.query(filterQuery);
+//                File oF = File.createTempFile("PROV-", ".ttl", Paths.get(".").toFile());
+//                try (BufferedWriter writer = Files.newBufferedWriter(oF.toPath())) {
+//                    writer.write(provTTL);
 //                }
-//
-//                Graph g = (Graph) maps.getGraph();
-//
-//                String mapsProvJson = "{ \"mappings\" : "
-//                        + JSONFormat.create(maps).toString()
-//                        + " , "
-//                        + "\"d3\" : "
-//                        + JSOND3Format.create(g).toString()
-//                        + " }";
-//                
-////                System.out.println(mapsProvJson);
-//
-//                String htmlOut = Util.genHtmlViz(mapsProvJson);
-//
-//                Path pathHtml = Files.createTempFile("provenanceDisplay-", ".html");
-//                Files.write(pathHtml, htmlOut.getBytes(), StandardOpenOption.WRITE);
-//                logger.info("HTML visualization written in " + pathHtml.toString());
-//                
-//                Path pathProv = Files.createTempFile("provenanceRDF-", ".ttl");
-//                Files.write(pathProv, provTTL.getBytes(), StandardOpenOption.WRITE);
-//                logger.info("RDF provenance written in " + pathProv.toString());
+//                logger.info("PROV written to " + Paths.get(oF.toURI()).normalize());
             } else {
                 // list histories
                 logger.info("Listing histories :");
@@ -204,7 +150,7 @@ public class Main {
                     Map<String, String> histories = gAPI.listHistories();
                     for (String id : histories.keySet()) {
                         String message = String.format("Found history with id %s and name %s", id, histories.get(id));
-                        System.out.println("\t"+message);
+                        System.out.println("\t" + message);
                     }
                 } catch (GalaxyProvenanceException ex) {
                     logger.error("An error occured while connecting to the Galaxy server. Please check the Galaxy server URL and your API key.");
@@ -217,9 +163,7 @@ public class Main {
             ex.printStackTrace();
             System.exit(1);
         } catch (ParseException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            logger.error("Impossible ti parse the command line arguments. Please read the help messages.");
         }
     }
 }
